9e60d5238c0cc7d2d369b23dca158c901b8efffb
Samuel <samuelalves681@gmail>
2022-12-27 00:18:21 -0300

    connect to infra

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './infra/app.controller';
-import { AppService } from './app.service';
-import { PrismaService } from './infra/prisma.service';
+import { HttpModule } from './infra/http/http.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [HttpModule],
 })
-export class AppModule { }
+export class AppModule {}
