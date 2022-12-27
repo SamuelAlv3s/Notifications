ef3e830a85ee9cb2de24e487210e51afa5a2f816
Samuel <samuelalves681@gmail>
2022-12-26 23:45:20 -0300

    add in memory database

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,24 +1,19 @@
 import { SendNotification } from './send-notification';
-import { Notification } from '../entities/notification'
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
 
-const notifications: Notification[] = []
-
-const notificationsRepository = {
-  async create(notification: Notification) {
-    notifications.push(notification);
-  },
-};
 
 describe('Send Notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification(notificationsRepository);
+    const notificationRepository = new InMemoryNotificationsRepository()
+    const sendNotification = new SendNotification(notificationRepository);
 
-    await sendNotification.execute({
+    const { notification } = await sendNotification.execute({
       content: 'Notification',
       category: 'info',
       recipientId: 'example-recipientId',
     });
 
-    expect(notifications).toHaveLength(1);
+    expect(notificationRepository.notifications).toHaveLength(1);
+    expect(notificationRepository.notifications[0]).toEqual(notification)
   });
 });
