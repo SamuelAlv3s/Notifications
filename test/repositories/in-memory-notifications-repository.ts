dba6263385ae4217e2f62a4b40359686694933e6
Samuel <samuelalves681@gmail>
2022-12-27 12:33:08 -0300

    configure typescript paths

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -1,9 +1,11 @@
-import { Notification } from "../../src/app/entities/notification";
-import { NotificationsRepository } from "../../src/app/repositories/notifications-repository";
+import { Notification } from '@app/entities/notification';
+import { NotificationsRepository } from '@app/repositories/notifications-repository';
 
-export class InMemoryNotificationsRepository implements NotificationsRepository {
-  public notifications: Notification[] = []
+export class InMemoryNotificationsRepository
+  implements NotificationsRepository
+{
+  public notifications: Notification[] = [];
   async create(notification: Notification) {
     this.notifications.push(notification);
   }
-};
+}
